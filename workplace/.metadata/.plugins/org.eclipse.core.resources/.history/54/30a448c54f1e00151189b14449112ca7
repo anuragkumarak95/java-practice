99aecd35b999dd6148ccccb8ba025cd4cfd1ebd7
anuragkumarak95 <anuragkumarak95@gmail.com>
2015-06-29 19:20:26 +0530

    using hibernate post pro up

---
+++ workplace/.metadata/.plugins/org.eclipse.core.resources/.history/54/30a448c54f1e00151189b14449112ca7
@@ -0,0 +1,18 @@
+package org.hib.prac;
+
+import org.hibernate.SessionFactory;
+
+public class first {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		UserDetail user = new UserDetail();
+		user.setName("Anurag");
+		user.setPass("123");
+		user.setId(1);
+		SessionFactory session = Configuration().configure().getSessionFactory();
+		
+		
+	}
+
+}
--- workplace/hibernateproj/src/org/hib/prac/UserDetail.java
+++ workplace/hibernateproj/src/org/hib/prac/UserDetail.java
@@ -1,14 +1,31 @@
 package org.hib.prac;
 
+import javax.persistence.Column;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
-@Entity
+@Entity(name="UserDetail")
 public class UserDetail {
-	@Id
+	@Id @GeneratedValue() //GeneratedVlaue specifically creates a sequencial incrementer for the below member variables.
 	private int id;
+	
+	@Column (name="USER_NAME") //Column annotation is used to specify some attributes of the available membervariable as the column in the hibernated table.
 	private String name;
+	
+	@Column (name="USER_PASS")
 	private String pass;
+	
+	@Embedded //this tag is used to speify that this member variable is to be embedded in the table as pure columns.
+	private Address address;
+	
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
 	public int getId() {
 		return id;
 	}
